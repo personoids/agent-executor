e77ca91a68324b6a9bdf17f258e05e4099039d01
Jonatan Russo <jonatan@mixtiles.com>
2023-06-05 15:20:13 +0300

    WIP: update prompts to fit personoids plugin

--- planner_prompt.py
+++ planner_prompt.py
@@ -3,51 +3,22 @@
 from langchain.prompts.prompt import PromptTemplate
 
 
-API_PLANNER_PROMPT = """You are a planner that plans a sequence of API calls to assist with user queries against an API.
+API_PLANNER_PROMPT = """You are a planner that plans the next API call to assist with user queries against an API.
 
 You should:
-1) evaluate whether the user query can be solved by the API documentated below. If no, say why.
-2) if yes, generate a plan of API calls and say what they are doing step by step.
-3) If the plan includes a DELETE call, you should always return an ask from the User for authorization first unless the User has specifically asked to delete something.
+1) evaluate which is the best method from the  API documentated below to solve the user query. 
+2) generate the API call
 
 You should only use API endpoints documented below ("Endpoints you can use:").
-You can only use the DELETE tool if the User has specifically asked to delete something. Otherwise, you should return a request authorization from the User first.
-Some user queries can be resolved in a single API call, but some will require several API calls.
 The plan will be passed to an API controller that can format it into web requests and return the responses.
 
 ----
 
 Here are some examples:
 
-Fake endpoints for examples:
-GET /user to get information about the current user
-GET /products/search search across products
-POST /users/{{id}}/cart to add products to a user's cart
-PATCH /users/{{id}}/cart to update a user's cart
-DELETE /users/{{id}}/cart to delete a user's cart
+User query: research how to use GCP through an npm package
+Plan: POST /webSearch with query to find information about Google Cloud Platform SDK.
 
-User query: tell me a joke
-Plan: Sorry, this API's domain is shopping, not comedy.
-
-User query: I want to buy a couch
-Plan: 1. GET /products with a query param to search for couches
-2. GET /user to find the user's id
-3. POST /users/{{id}}/cart to add a couch to the user's cart
-
-User query: I want to add a lamp to my cart
-Plan: 1. GET /products with a query param to search for lamps
-2. GET /user to find the user's id
-3. PATCH /users/{{id}}/cart to add a lamp to the user's cart
-
-User query: I want to delete my cart
-Plan: 1. GET /user to find the user's id
-2. DELETE required. Did user specify DELETE or previously authorize? Yes, proceed.
-3. DELETE /users/{{id}}/cart to delete the user's cart
-
-User query: I want to start a new cart
-Plan: 1. GET /user to find the user's id
-2. DELETE required. Did user specify DELETE or previously authorize? No, ask for authorization.
-3. Are you sure you want to delete your cart? 
 ----
 
 Here are endpoints you can use. Do not reference any of the endpoints above.
@@ -59,11 +30,11 @@ Here are endpoints you can use. Do not reference any of the endpoints above.
 User query: {query}
 Plan:"""
 API_PLANNER_TOOL_NAME = "api_planner"
-API_PLANNER_TOOL_DESCRIPTION = f"Can be used to generate the right API calls to assist with a user query, like {API_PLANNER_TOOL_NAME}(query). Should always be called before trying to call the API controller."
+API_PLANNER_TOOL_DESCRIPTION = f"Can be used to generate the next API call to assist with a user query, like {API_PLANNER_TOOL_NAME}(query). Should always be called before trying to call the API controller."
 
 # Execution.
-API_CONTROLLER_PROMPT = """You are an agent that gets a sequence of API calls and given their documentation, should execute them and return the final response.
-If you cannot complete them and run into issues, you should explain the issue. If you're able to resolve an API call, you can retry the API call. When interacting with API objects, you should extract ids for inputs to other API calls but ids and names for outputs returned to the User.
+API_CONTROLLER_PROMPT = """You are an agent that gets an API call and given its documentation, execute it and return the final response.
+
 
 
 Here is documentation on the API:
@@ -100,15 +71,15 @@ API_CONTROLLER_TOOL_DESCRIPTION = f"Can be used to execute a plan of API calls, 
 # The goal is to have an agent at the top-level (e.g. so it can recover from errors and re-plan) while
 # keeping planning (and specifically the planning prompt) simple.
 API_ORCHESTRATOR_PROMPT = """You are an agent that assists with user queries against API, things like querying information or creating resources.
-Some user queries can be resolved in a single API call, particularly if you can find appropriate params from the OpenAPI spec; though some require several API calls.
+all user queries can be resolved in a single API call. you need to find the most appropriate parameters from the OpenAPI spec
 You should always plan your API calls first, and then execute the plan second.
-If the plan includes a DELETE call, be sure to ask the User for authorization first unless the User has specifically asked to delete something.
 You should never return information without executing the api_controller tool.
-
+Whenever bootstrap is mentioned in the user query, you should first call the /bootstrapPlugin endpoint
 
 Here are the tools to plan and execute API requests: {tool_descriptions}
 
 
+Use the format below for all of the responses, including failures
 Starting below, you should follow this format:
 
 User query: the query a User wants help with related to the API
@@ -122,19 +93,16 @@ Final Answer: the final output from executing the plan
 
 
 Example:
-User query: can you add some trendy stuff to my shopping cart.
-Thought: I should plan API calls first.
+User query: can you find me some trendy stuff
+Thought: I should plan an API call first.
 Action: api_planner
-Action Input: I need to find the right API calls to add trendy items to the users shopping cart
-Observation: 1) GET /items with params 'trending' is 'True' to get trending item ids
-2) GET /user to get user
-3) POST /cart to post the trending items to the user's cart
+Action Input: I need to find the right API call to find trendy items
+Observation: GET /items with params 'trending' is 'True' to get trending item ids
 Thought: I'm ready to execute the API calls.
 Action: api_controller
-Action Input: 1) GET /items params 'trending' is 'True' to get trending item ids
-2) GET /user to get user
-3) POST /cart to post the trending items to the user's cart
+Action Input: GET /items params 'trending' is 'True' to get trending item ids
 ...
+
 
 Begin!
 
